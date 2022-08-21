65bfa73550154bc6ef68b9631621888c3dbfeb3d
it21781 <it21781@hua.gr>
2022-08-21 18:22:34 +0300

    -

--- relationships.py
+++ relationships.py
@@ -1,6 +1,5 @@
 from neo4j import GraphDatabase
 import haversine as hs
-import re
 
 results = []
 
@@ -38,18 +37,22 @@ class Neo4j:
             lon1 = node.get('n.longitude')
             loc1 = (lat1, lon1)
             for node2 in second:
-                id2 = node2.get('n.id')
-                lat2 = node2.get('n.latitude')
-                lon2 = node2.get('n.longitude')
-                loc2 = (lat2, lon2)
-                kms = hs.haversine(loc1, loc2)
-                print(round(kms, 2))
+                if node2 != node:
+                    id2 = node2.get('n.id')
+                    lat2 = node2.get('n.latitude')
+                    lon2 = node2.get('n.longitude')
+                    loc2 = (lat2, lon2)
+                    kms = hs.haversine(loc1, loc2)
+                    dist = round(kms, 2)
+                    order2 = 'match (a),(b) where a.id="'+id1+'"and b.id="'+id2+'"create (a)-[r:Distance {name:'+str(dist)+'}]->(b)'
+                    db_connector.add_relationships(order2)
+                else:
+                    pass
 
 
 if __name__ == "__main__":
-    borough = 'Staten Island'
+    borough = 'Staten Island'   # insert manually borough name to split one huge process
     order = 'match (n) where n.locality="' + borough + '"return distinct n.id, n.latitude, n.longitude '
-    # order2 = 'match (a),(b) where a.id = and b.id = create (a)-[r:Distance {name:'pntl'}]->(b)'
     db_connector = Neo4j("bolt://localhost:7687", "neo4j", "pass123")
     db_connector.get_results(order)
     db_connector.close()
